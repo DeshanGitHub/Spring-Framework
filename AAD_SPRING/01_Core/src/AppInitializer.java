c479504631204ba1ab65c3f7bacafbff7135da49
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-06 05:34:03 +0530

    Application Context Checking

--- AAD_SPRING/01_Core/src/AppInitializer.java
+++ AAD_SPRING/01_Core/src/AppInitializer.java
@@ -1,12 +1,26 @@
+import bean.SpringBean;
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
 
+        /*Apita Spring wala siyaluma weda karala dena container eka creat*/
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+
+        /*App config class eken object ekak create kirima*/
         ctx.register(AppConfig.class);
+
         ctx.refresh();
+
+        /*spring bean class eke tiyena ewa object ekak hadala methanata ganna puluwanda kiyala balana eka*/
+        SpringBean bean = ctx.getBean(SpringBean.class);
+
+        /*Api hadapu bean eken method ekak cl karala belima*/
+        bean.testBean();
+
+        System.out.println(bean);
+
         ctx.close();
 
     }
