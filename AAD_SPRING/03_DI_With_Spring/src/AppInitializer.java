723b7ba5847a5c63744850962388afde06731146
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-06 16:21:16 +0530

    Dipendancy Injection using @Autowired

---
+++ AAD_SPRING/03_DI_With_Spring/src/AppInitializer.java
@@ -0,0 +1,19 @@
+import bean.Boy;
+import config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+        context.registerShutdownHook();
+
+
+        Boy boy = context.getBean(Boy.class);
+        boy.chattingWithGirl();
+//        Girl girl = context.getBean(Girl.class);
+
+
+    }
+}
---
+++ AAD_SPRING/03_DI_With_Spring/src/bean/GoodGirl.java
@@ -0,0 +1,6 @@
+package bean;
+
+public interface GoodGirl {
+    void chat();
+}
+
