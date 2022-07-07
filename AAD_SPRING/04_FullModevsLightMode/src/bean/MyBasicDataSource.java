bfd93aa63bff962657b93b055e16d59596dbb2fb
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-07 10:55:53 +0530

    Introduced Lighy Mode

--- AAD_SPRING/04_FullModevsLightMode/src/bean/MyBasicDataSource.java
+++ AAD_SPRING/04_FullModevsLightMode/src/bean/MyBasicDataSource.java
@@ -1,9 +1,43 @@
 package bean;
 
-public class MyBasicDataSource {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class MyBasicDataSource implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     MyConnection connection;
+
+    public MyBasicDataSource() {
+        System.out.println("MyBasicDataSource Instantiated");
+    }
 
     public void setMyConnection(MyConnection connection) {
         this.connection = connection;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("MyBasicDataSource Bean Factory Aware");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("MyBasicDataSource Bean Name Aware");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("MyBasicDataSource Disposable Bean");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("MyBasicDataSource Initializing Bean");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("MyBasicDataSource Application Context Aware");
     }
 }
--- AAD_SPRING/04_FullModevsLightMode/src/bean/MyConnection.java
+++ AAD_SPRING/04_FullModevsLightMode/src/bean/MyConnection.java
@@ -1,4 +1,38 @@
 package bean;
 
-public class MyConnection {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class MyConnection implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+
+    public MyConnection() {
+        System.out.println("MyConnection Instantiated");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("MyConnection Bean Factory Aware");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("MyConnection Bean Name Aware");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("MyConnection Disposable Bean");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("MyConnection Initializing Bean");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("MyConnection Application Context Aware");
+    }
 }
