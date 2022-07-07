eaed5eea5a8682f00e9d3c44b48523f52b820e65
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-07 12:43:34 +0530

    Spring Configurations Introduced using @Import Annotation

--- AAD_SPRING/05_Configurations/src/config/AppConfig.java
+++ AAD_SPRING/05_Configurations/src/config/AppConfig.java
@@ -2,11 +2,13 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 
 //A source for Spring Bean Definitions
 @Configuration
 @ComponentScan(basePackages = "bean")
+@Import({AppConfig1.class, AppConfig2.class})
 public class AppConfig {
 
 }
