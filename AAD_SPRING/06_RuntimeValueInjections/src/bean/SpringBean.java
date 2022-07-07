8ff6e5299fc022ab97a4075f11ff594107e5c126
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-07 14:53:33 +0530

    Introduced to use @Autowired(required = false)

--- AAD_SPRING/06_RuntimeValueInjections/src/bean/SpringBean.java
+++ AAD_SPRING/06_RuntimeValueInjections/src/bean/SpringBean.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringBean {
 
-    @Autowired
+    @Autowired(required = false)
     public SpringBean(@Value("C001") String id, @Value("28") int age, @Value("true") boolean b) {
         System.out.println("Spring bean one Instantiated");
         System.out.println(id);
@@ -15,7 +15,8 @@ public class SpringBean {
         System.out.println(b);
     }
 
-    public SpringBean(@Value("5") int num, @Value("Deshan") String myName, @Value("Kamal, Amara, Sampath") String[] names) {
+    @Autowired(required = false)
+    public SpringBean(@Value("5") int num, @Value("Deshan") String myName, @Value("Kamal, Amara, Sampath") String[] names, @Value("26.8") double amount) {
         System.out.println("Spring Bean Instantiated");
         System.out.println(num);
         System.out.println(myName);
@@ -25,5 +26,7 @@ public class SpringBean {
         ) {
             System.out.println(name);
         }
+
+        System.out.println(amount);
     }
 }
