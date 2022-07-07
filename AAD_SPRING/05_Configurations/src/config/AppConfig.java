eb26d099b0ba380d41c0bae7f115731a7411f198
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-07 13:06:56 +0530

    Introduce to add XML files

--- AAD_SPRING/05_Configurations/src/config/AppConfig.java
+++ AAD_SPRING/05_Configurations/src/config/AppConfig.java
@@ -3,12 +3,15 @@ package config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 
 //A source for Spring Bean Definitions
 @Configuration
 @ComponentScan(basePackages = "bean")
 @Import({AppConfig1.class, AppConfig2.class})
+//@ImportResource("classpath:abcd.xml")//We can use this annotation to import xml configurations to this project
+//@ImportResource("file:absolute-path-of-abcd.xml")
 public class AppConfig {
 
 }
