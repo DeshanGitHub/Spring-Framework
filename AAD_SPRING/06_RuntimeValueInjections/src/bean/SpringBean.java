0bd142f13031cb01be658059f6effe97eb1de046
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-07 14:34:42 +0530

    Introduce to Use @Autowired annotation to create parameterized constructor

--- AAD_SPRING/06_RuntimeValueInjections/src/bean/SpringBean.java
+++ AAD_SPRING/06_RuntimeValueInjections/src/bean/SpringBean.java
@@ -1,10 +1,19 @@
 package bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
+
+    @Autowired
+    public SpringBean(@Value("C001") String id, @Value("28") int age, @Value("true") boolean b) {
+        System.out.println("Spring bean one Instantiated");
+        System.out.println(id);
+        System.out.println(age);
+        System.out.println(b);
+    }
 
     public SpringBean(@Value("5") int num, @Value("Deshan") String myName, @Value("Kamal, Amara, Sampath") String[] names) {
         System.out.println("Spring Bean Instantiated");
