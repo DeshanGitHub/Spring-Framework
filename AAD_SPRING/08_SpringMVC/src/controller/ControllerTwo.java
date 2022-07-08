eaa45b1f3777e6237417bf8e9ed1cf53f24dae1b
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-08 18:23:41 +0530

    Content Update

--- AAD_SPRING/08_SpringMVC/src/controller/ControllerTwo.java
+++ AAD_SPRING/08_SpringMVC/src/controller/ControllerTwo.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("two")
+@RequestMapping("two") // Handler method
 public class ControllerTwo {
 
+    //Mapping methods // Handler Methods
     @GetMapping
     public String testTwo() {
         return "Hello Spring";
