524fc88bad6dbae1134f632ef811de27d5c24c8e
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-09 08:58:43 +0530

    Set Path to Same Handler Mappings

--- AAD_SPRING/09_RestControllers/src/lk/ijse/spring/controller/CustomerController.java
+++ AAD_SPRING/09_RestControllers/src/lk/ijse/spring/controller/CustomerController.java
@@ -12,6 +12,16 @@ public class CustomerController {
         return "Get All Customers Invoked and Get Method Invoked";
     }
 
+    @GetMapping(path = "search") //handler mapping
+    public String searchCustomer() { // handler methods
+        return "Search Customers Invoked and Get Method Invoked";
+    }
+
+    @GetMapping(path = "newid") //handler mapping
+    public String generateNewID(){ // handler methods
+        return "New Customers ID Invoked and Get Method Invoked";
+    }
+
     @PostMapping  //handler mapping
     public String saveCustomer() { // handler methods
         return "Customer Saved and Post Method Invoked";
