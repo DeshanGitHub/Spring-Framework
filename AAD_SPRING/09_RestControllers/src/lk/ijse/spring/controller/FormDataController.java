f17ff38ba017943470d14dc4837f31d209dc4adb
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-10 08:45:17 +0530

    Introduced Form Data Controller

---
+++ AAD_SPRING/09_RestControllers/src/lk/ijse/spring/controller/FormDataController.java
@@ -0,0 +1,19 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("form")
+public class FormDataController {
+
+    @PostMapping
+    public String testForm1(@ModelAttribute CustomerDTO dto) {
+        return "Request Received " + dto.toString();
+    }
+
+
+}
