201c59fd93f914571e2ece537d9bb28248472746
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-15 10:13:57 +0530

    Using Assertion methods for testing

---
+++ AAD_SPRING/13_Model_Mapper/src/test/java/lk/ijse/spring/service/impl/CustomerServiceImplTest.java
@@ -0,0 +1,54 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebAppConfiguration
+@ContextConfiguration(classes = {WebAppConfig.class})
+@ExtendWith(SpringExtension.class)
+@Transactional
+class CustomerServiceImplTest {
+    @Autowired
+    CustomerService customerService;
+
+    @Test
+    void saveCustomer() {
+        //If it is a new Customer It should added to the database
+        CustomerDTO customerDTO1 = new CustomerDTO("C020", "Ramal", "Galle", 1000.00);
+        customerService.saveCustomer(customerDTO1);
+
+
+        // If the customer already exist. check if it is throwing the error
+        CustomerDTO customerDTO2 = new CustomerDTO("C020", "Ramal", "Galle", 1000.00);
+        // Check if there is a error
+        assertThrows(RuntimeException.class, () -> {
+            customerService.saveCustomer(customerDTO2);
+        });
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void searchCustomer() {
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+}
