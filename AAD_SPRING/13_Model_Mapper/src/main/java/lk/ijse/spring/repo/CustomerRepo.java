7c77a39fb2bc2c884b8137bf0e13039c7087ce23
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-16 08:49:27 +0530

    Aded Count By And aded Exist By for testing

--- AAD_SPRING/13_Model_Mapper/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
+++ AAD_SPRING/13_Model_Mapper/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -3,6 +3,8 @@ package lk.ijse.spring.repo;
 import lk.ijse.spring.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepo extends JpaRepository<Customer, String> {
     //query methods
     Customer findCustomerByName(String name);
@@ -10,4 +12,24 @@ public interface CustomerRepo extends JpaRepository<Customer, String> {
     Customer findCustomerByAddress(String address);
 
     Customer findCustomerByNameAndAddress(String name,String address);
+
+    Customer findByName(String name);
+
+    Customer readByName(String name);
+
+    Customer getByName(String name);
+
+    Customer queryByName(String name);
+
+//    Customer searchByName(String name);
+
+    Customer streamByName(String name);
+
+    List<Customer> searchByName(String name);
+
+    //test countBy
+    long countByName(String name);
+
+    //test existBy
+    boolean existsByNameAndAddress(String name,String address);
 }
--- AAD_SPRING/13_Model_Mapper/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ AAD_SPRING/13_Model_Mapper/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.List;
+
 @WebAppConfiguration // State test configuration class
 @ContextConfiguration(classes = {JPAConfig.class}) // import configurations for Test Context
 @ExtendWith(SpringExtension.class)
@@ -20,7 +22,9 @@ class CustomerRepoTest {
     @Test
     public void searchCustomerByName() {
         Customer c1 = customerRepo.findCustomerByName("Kasun");
+        Customer c2 = customerRepo.findByName("Kasun");
         System.out.println(c1.toString());
+        System.out.println(c2.toString());
     }
 
     @Test
@@ -33,6 +37,47 @@ class CustomerRepoTest {
     public void searchCustomerByNameAndAddress() {
         Customer c1 = customerRepo.findCustomerByNameAndAddress("Kasun", "Kaluthara");
         System.out.println(c1.toString());
+    }
+
+    @Test
+    public void searchByName() {
+        List<Customer> customers = customerRepo.searchByName("Dasun");
+        for (Customer customer : customers) {
+            System.out.println(customer.toString());
+        }
+    }
+
+    @Test
+    public void t1() {
+        Customer c1 = customerRepo.findByName("Dasun");
+        System.out.println(c1.toString());
+
+        Customer c2 = customerRepo.readByName("Dasun");
+        System.out.println(c2.toString());
+
+        Customer c3 = customerRepo.getByName("Dasun");
+        System.out.println(c3.toString());
+
+        Customer c4 = customerRepo.queryByName("Dasun");
+        System.out.println(c4.toString());
+
+//        Customer c5 = customerRepo.searchByName("Kasun");
+//        System.out.println(c5.toString());
+
+        Customer c6 = customerRepo.streamByName("Kasun");
+        System.out.println(c6.toString());
+    }
+
+    @Test
+    public void testCountBy(){
+        long ramal = customerRepo.countByName("Kamal");
+        System.out.println(ramal);
+    }
+
+    @Test
+    public void testExistBy(){
+        boolean b = customerRepo.existsByNameAndAddress("Dasun", "Panadura");
+        System.out.println(b);
     }
 
 }
