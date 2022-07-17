ac953538258276eaab1110e96ec208d913cbef99
Kavindu Deshan <kkavindu.deshan@gmail.com>
2022-07-17 06:32:59 +0530

    Added Item pos functions

--- AAD_SPRING/13_Model_Mapper/src/main/java/lk/ijse/spring/dto/ItemDTO.java
+++ AAD_SPRING/13_Model_Mapper/src/main/java/lk/ijse/spring/dto/ItemDTO.java
@@ -1,23 +1,17 @@
-package lk.ijse.spring.entity;
+package lk.ijse.spring.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @ToString
-public class Item {
-    @Id
+public class ItemDTO {
     private String code;
     private String description;
     private int qtyOnHand;
     private double unitPrice;
-
 }
